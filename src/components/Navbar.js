4d9649d05c96459ac32a0973c2367392d7c2c369
Qulam2004 <qqulamov96@gmail.com>
2021-07-02 04:31:57 +0400

    Navbar success

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,12 @@ function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <a className="navbar-brand" href="#"><img className="logo" src={NavbarLogo}></img></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          
         </button>      
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a className="nav-link text-primary actived" href="#">ANA SƏHİFƏ</a>
+              <a className="nav-link active text-primary " href="#">ANA SƏHİFƏ</a>
             </li>
             <li className="nav-item">
               <a className="nav-link text-primary" href="#">HAQQIMDA</a>
